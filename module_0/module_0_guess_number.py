f95fbf88da899b360de949fab0318d373d55a88c
minsar1101 <minsar@gmail.com>
2020-05-31 20:06:12 +0300

    boundary parameters + pep8
    1) default boundary params, size added to functions
    2) pep8 fixes

--- module_0/module_0_guess_number.py
+++ module_0/module_0_guess_number.py
@@ -1,36 +1,46 @@
 # -*- coding: utf-8 -*-
 """
 SkillFactory DataScience module 0 task
-version: 0.1
+version: 0.2
 Created on Sun May 31 19:20:55 2020
 @author: insar
 """
 
-def game_core_v3(number):
+
+def game_core_v3(number, left_boundary, right_boundary):
+    '''
+    Начинаем с числа посередине заданного промежутка. Сравниваем с загаданным числом.
+    В зависимости от результата сравнения берем одну из половинок - и снова берем
+    число посередине.
+    '''
     count = 1
-    left_boundary = 0
-    right_boundary = 100
-    predict = int((right_boundary - left_boundary)/2)
+    predict = int((right_boundary - left_boundary)/2)  # в первый раз берем число посередине.
 
     while predict != number:
         if predict < number:
             left_boundary = predict+1
         elif predict > number:
             right_boundary = predict-1
+        # выбираем промежуток, в который попадает загаданное число
         predict = left_boundary + int((right_boundary - left_boundary)/2)
         count += 1
     return(count)
-        
-        
-def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+
+
+def score_game(game_core, left_boundary=1, right_boundary=100, size=1000):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число
+    left_boundary - нижняя граница возможного числа,
+    right_boundary - правая граница загаданного числа,
+    size - количество экспериментов.
+    '''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1,101, size=(1000))
+    random_array = np.random.randint(left_boundary, right_boundary+1, size)
     for number in random_array:
-        count_ls.append(game_core(number))
+        count_ls.append(game_core(number, left_boundary, right_boundary))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
+
 
 score_game(game_core_v3)
